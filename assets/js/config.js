2aab1514a4cf2fbb86431ccab74f64167b986620
2f-ณัฐ <natchanon@2fellows.com>
2026-06-03 03:38:00 +0700

    update app version5.3.2 building3

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
     version: '5.3.2',
-    build: '20260603.2',
+    build: '20260603.3',
 };
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thaihelp-plus-v5.3.2-build-20260603.2';
+const CACHE_NAME = 'thaihelp-plus-v5.3.2-build-20260603.3';
 const urlsToCache = [
     './',
     './index.html',
